a62aafc62c6d57f63867962d6c7218cb99d6ce1f
Thomas Koch <koch@comsolit.ch>
2014-01-29 16:28:06 +0100

    filter items by priority

--- files/comsolitbacklog.js
+++ files/comsolitbacklog.js
@@ -5,6 +5,17 @@
     $scope.items = getEmbeddedJSON('backlogItems');
   });
 
+  comsolitBacklog.filter('prioritizedItems', function(){
+    var filters = {
+      'false': function(x){return 0.0 === parseFloat(x.backlog_position)},
+      'true': function(x){return parseFloat(x.backlog_position) > 0}
+    };
+    return function(items, prioritized){
+      var filter = filters[prioritized];
+      return items.filter(filter, items);
+    };
+  });
+
   function getEmbeddedJSON(name) {
     var context = arguments.length === 2 ? arguments[1] : 'body';
 
--- templates/backlog.php
+++ templates/backlog.php
@@ -1,4 +1,25 @@
 <div ng-app="comsolitBacklog" ng-controller="comsolitBacklogCtrl">
+
+  <h4><?php echo plugin_lang_get( 'prioritized_items' )?></h4>
+
+  <div style="border: 1px solid black; min-height: 10em;">
+    <table class="width100" cellspacing="1" >
+      <thead>
+        <tr>
+          <th>summary</th>
+          <th>backlog_position</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr ng-repeat="item in items|prioritizedItems:true" style="background-color: #ffcd85">
+          <td>{{item.summary}}</td>
+          <td>{{item.backlog_position}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <h4><?php echo plugin_lang_get( 'unprioritized_items' )?></h4>
   <table class="width100" cellspacing="1" >
     <thead>
       <tr>
@@ -7,7 +28,7 @@
       </tr>
     </thead>
     <tbody>
-      <tr ng-repeat="item in items" style="background-color: #ffcd85">
+      <tr ng-repeat="item in items|prioritizedItems:false" style="background-color: #ffcd85">
         <td>{{item.summary}}</td>
         <td>{{item.backlog_position}}</td>
       </tr>
