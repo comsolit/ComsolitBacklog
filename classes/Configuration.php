3805e7b44649be5127dcd89bec9a7ff7418cefa7
Thomas Koch <koch@comsolit.ch>
2014-02-06 14:12:53 +0100

    fixes and extensions in Configuration class

--- classes/Configuration.php
+++ classes/Configuration.php
@@ -2,36 +2,52 @@
 
 class Configuration {
 
+    const GLOBAL_PREFIX = 'g_comsolitBacklog_';
+
     private static $configNames = array(
         'prioritizedStatus',
-        'unprioritizedStatus'
+        'unprioritizedStatus',
+        'categories'
     );
 
     private $config = array();
 
     public function __construct(array $config) {
-        foreach($config as $key => $value) {
-            $this->ensureValidName($name);
+        foreach($config as $name => $value) {
+            self::ensureValidName($name);
             $this->config[$name] = $value;
         }
     }
 
     public function get($name) {
-        $this->ensureValidName($name);
+        self::ensureValidName($name);
         if(!array_key_exists($name, $this->config)) return null;
         return $this->config[$name];
+    }
+
+    public function getRequired($name) {
+        $value = $this->get($name);
+        if(is_null($value)) throw new \Exception('missing configuration: ' . $name . ' set global variable $' . self::globalVarName($name));
+        return $value;
     }
 
     public static function fromGlobalVariables() {
         $config = array();
         foreach(self::$configNames as $name) {
-            $globalsName = 'g_comsolitBacklog_' . $name;
-            if(array_key_exists($nglobalsName, $GLOBALS)) $config[$name] = $GLOBALS[$globalsName];
+            if(self::isSetGlobal($name)) $config[$name] = $GLOBALS[self::globalVarName($name)];
         }
         return new self($config);
     }
 
-    private function ensureValidName($name) {
+    private static function ensureValidName($name) {
         if(!in_array($name, self::$configNames)) throw new \Exception('unknown config name: ' .  $name);
+    }
+
+    private static function globalVarName($name) {
+        return self::GLOBAL_PREFIX . $name;
+    }
+
+    private static function isSetGlobal($name) {
+        return array_key_exists(self::globalVarName($name), $GLOBALS);
     }
 }
