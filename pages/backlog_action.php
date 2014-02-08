a0396f7c3ae08fc08ec637f3d686987d3cd45373
Thomas Koch <thomas@koch.ro>
2014-02-08 19:38:02 +0100

    add license headers

--- pages/backlog_action.php
+++ pages/backlog_action.php
@@ -1,4 +1,18 @@
 <?php
+// Copyright 2014 comsolit AG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use Comsolit\Backlog\ActionData;
 use Comsolit\Backlog\BacklogUpdateController;
@@ -35,12 +49,12 @@ function getComsolitBacklogActionIO() {
 
 function comsolitBacklogActionDo() {
     if($_SERVER['REQUEST_METHOD'] !== 'POST') throw new Exception('only POST requests allowed');
-    
+
     $rawPostData = file_get_contents('php://input');
     $actionData = new ActionData($rawPostData);
     $configuration = Configuration::fromGlobalVariables();
     $projectId = 1; // TODO!!!!!!!!!!!!
-    
+
     return BacklogUpdateController::run($actionData, Backlog::fromGlobalData());
 }
 
